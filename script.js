f957ba62b4f488eee6fcc0a276efdf84e34437ea
Ramesh Ramasamy <75352331+rameshramasamy1989@users.noreply.github.com>
2025-03-18 20:14:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,12 +15,18 @@ domReady(function () {
 
     // If found you qr code
     function onScanSuccess(decodeText, decodeResult) {
-        alert("You Qr is : " + decodeText, decodeResult);
+        parsed_url = new URL(decodeText);
+        query_params = Object.fromEntries(new URLSearchParams(parsed_url.search));
+        mc_value = query_params.mc || null;
+        alert("MCC is : " + mc_value);
+
+        //alert("You Qr is : " + decodeText, decodeResult);
+
     }
 
     let htmlscanner = new Html5QrcodeScanner(
         "my-qr-reader",
-        { fps: 10, qrbos: 250 }
+        { fps: 10, qrbos: 250,facingMode: { exact: "environment" } }
     );
     htmlscanner.render(onScanSuccess);
 });
