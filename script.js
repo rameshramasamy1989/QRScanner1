f946c928d3b073e5328067e6bc78d652ad738b22
Ramesh Ramasamy <75352331+rameshramasamy1989@users.noreply.github.com>
2025-03-18 20:37:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,29 +19,14 @@ domReady(function () {
         query_params = Object.fromEntries(new URLSearchParams(parsed_url.search));
         mc_value = query_params.mc || null;
         alert("MCC is : " + mc_value);
-        html5QrcodeScanner.clear();
 
         //alert("You Qr is : " + decodeText, decodeResult);
 
     }
 
-   //  let htmlscanner = new Html5QrcodeScanner(
- //       "my-qr-reader",
- //       { fps: 10, qrbos: 250, start: true, facingMode: "environment" }
-  //  );
-   // htmlscanner.render(onScanSuccess);
-
-    const html5QrCode = new Html5Qrcode("my-qr-reader");
-const qrCodeSuccessCallback = (decodedText, decodedResult) => {
-    /* handle success */
-    parsed_url = new URL(decodeText);
-        query_params = Object.fromEntries(new URLSearchParams(parsed_url.search));
-        mc_value = query_params.mc || null;
-        alert("MCCxx code is : " + mc_value);
-};
-const config = { fps: 10, qrbox: { width: 250, height: 250 } };
-
-
-// If you want to prefer back camera
-html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback);
+    let htmlscanner = new Html5QrcodeScanner(
+        "my-qr-reader",
+        { fps: 10, qrbos: 250,rememberLastUsedCamera: true,showTorchButtonIfSupported: true }
+    );
+    htmlscanner.render(onScanSuccess);
 });
