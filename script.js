9a9505d64d464a003e0b3a5a345429839ddf59e6
Ramesh Ramasamy <75352331+rameshramasamy1989@users.noreply.github.com>
2025-03-18 20:38:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ domReady(function () {
 
     let htmlscanner = new Html5QrcodeScanner(
         "my-qr-reader",
-        { fps: 10, qrbos: 250,rememberLastUsedCamera: true,showTorchButtonIfSupported: true }
+        { fps: 10, qrbos: 250 }
     );
     htmlscanner.render(onScanSuccess);
 });
